3c17a4564a29ee460ee19a437046b07f5888b89d
Cybernuki <1164@holbertonschool.com>
2019-10-29 20:21:41 -0500

    Print integers

--- format_d.c
+++ format_d.c
@@ -6,9 +6,30 @@
  */
 int format_d(va_list arg)
 {
-	int d;
+	int n, i, power;
 
-	d = va_arg(arg, int);
-	
-	return (d);
+	n = va_arg(arg, int);
+	power = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	while ((n / power) > 9 || (n / power) < -9)
+	{
+		power *= 10;
+	}
+	for (i = power; i >= 1 || i <= -1; i /= 10)
+	{
+		if (n < 0)
+		{
+			_putchar((n / i) % 10 * -1 + '0');
+		}
+		else
+		{
+			_putchar((n / i) % 10 + '0');
+		}
+	}
+	return (0);
 }
--- format_i.c
+++ format_i.c
@@ -6,9 +6,30 @@
  */
 int format_i(va_list arg)
 {
-	int i;
+	int n, i, power;
 
-	i = va_arg(arg, int);
+	n = va_arg(arg, int);
+	power = 1;
 
-	return (i);
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+
+	while ((n / power) > 9 || (n / power) < -9)
+	{
+		power *= 10;
+	}
+	for (i = power; i >= 1 || i <= -1; i /= 10)
+	{
+		if (n < 0)
+		{
+			_putchar((n / i) % 10 * -1 + '0');
+		}
+		else
+		{
+			_putchar((n / i) % 10 + '0');
+		}
+	}
+	return (0);
 }
