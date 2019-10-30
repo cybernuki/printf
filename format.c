6b86ee086e757a63d46feb355bc14e03d78bdb92
jhonatan arenas <1164@holbertonschool.com>
2019-10-30 14:44:42 +0000

    merge solving

--- format.c
+++ format.c
@@ -10,57 +10,6 @@
  */
 int formating(const char *p, int *index, va_list arg)
 {
-<<<<<<< HEAD
-	int j, start = *index;
-   
-	specifier spec[] = {
-        {'c', format_c},
-        {'s', format_s},
-	{'i', format_number},
-	{'d', format_number},
-        {'\0', NULL}
-    };
-	if (p[*index] != '%')
-    {
-	    while (p[*index] != '%' && p[*index] != '\0')
-	    {
-            *index = *index + 1;
-        }
-	    return (write(1, p + start, *index - start));
-    }
-	if (p[*index + 1] == '\0')
-    {
-	    *index = *index + 1;
-	    return (-1);
-    }
-	*index = *index + 1;
-    if (p[*(index)] == '%')
-    {
-	    int tmp = write (1, p + start, *index - start);
-	    *index = *index + 1;
-	    return(tmp);
-    }
-    if (p[*index] != '\0')
-    {
-        j = 0;
-        while (spec[j].letter != '\0')
-        {
-            if (p[*index] == spec[j].letter)
-            {
-		    *index = *index + 1;
-		    return(spec[j].f(arg));
-            }
-            j++;
-        }
-
-        *index = *index + 1;
-    }
-    while (p[*index] != '%' && p[*index] != '\0')
-        {
-            *index = *index + 1;
-        }
-    return (write (1, p + start, *index - start));
-=======
 	int start = *index;
 	/*  format_factory */
 	specifier spec[] = {
@@ -151,5 +100,5 @@ void find_end(const char *p, int *index)
 	{
 		*index = *index + 1;
 	}
->>>>>>> 31678e419a42fb904f784fcbe85786f0f433979e
+
 }
